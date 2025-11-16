4285640c5576be9801e45cb0bc906530c5f60e5e
Christian Eberle <christian.eberle.78@gmx.de>
2025-11-16 21:59:00 +0100

    After unit 19. Equality for All

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -3,6 +3,7 @@ package guru.springframework;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class MoneyTest {
 
@@ -14,4 +15,11 @@ class MoneyTest {
         product = five.times(3);
         assertEquals(15, product.amount);
     }
+
+    @Test
+    void testEquality() {
+        assertEquals(new Dollar(5), new Dollar(5));
+        assertNotEquals(new Dollar(5), new Dollar(8));
+    }
+
 }
